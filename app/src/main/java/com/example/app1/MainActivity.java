3f01877fbd5fdaf2353e0c90eb71fca6209b630f
zzynth <javier.salgado1478@gmail.com>
2025-09-19 21:17:21 -0300

    avance funciones-no completo

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.app1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Enlaza el botón por ID
+        Button btnEmpezar = findViewById(R.id.btnEmpezar);
+
+        //Agrega el listener para cambiar de actividad
+        btnEmpezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainInicio.class);
+                startActivity(intent);
+                finish(); //para cerrar la pantalla actual
+            }
+        });
     }
 }
--- app/src/main/java/com/example/app1/MainInicio.java
+++ app/src/main/java/com/example/app1/MainInicio.java
@@ -1,12 +1,15 @@
 package com.example.app1;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainInicio extends AppCompatActivity {
 
@@ -15,10 +18,30 @@ public class MainInicio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_inicio);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnIniciarSesion = findViewById(R.id.btnLogin1);
+        Button btnRegistrar = findViewById(R.id.btnRegister1);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainInicio.this, MainIniciarSesion.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainInicio.this, Mainregister.class);
+                startActivity(intent);
+            }
         });
     }
 }
